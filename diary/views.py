ffc7085d4fd94969aafe5f566556dd4e7b0366a5
yutagood <bzbzbzwell@i.softbank.jp>
2024-09-20 18:25:08 +0900

    “トップページ作成、修正”

--- diary/views.py
+++ diary/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
-
 class IndexView(View):
     def get(self, request):
         return render(request, "diary/index.html")
-
 
 index = IndexView.as_view()
