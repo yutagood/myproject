2568a16935c7b705e4242d5b68def13e2f40a8cb
yutagood <bzbzbzwell@i.softbank.jp>
2024-09-26 06:06:18 +0900

    “datetime追加”

--- diary/views.py
+++ diary/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
 
 class IndexView(View):
     def get(self, request):
-        return render(request, "diary/index.html")
+        datetime_now = datetime.now(
+            ZoneInfo("Asia/Tokyo")
+        ).strftime("%Y年%m月%d日 %H:%M:%S")
+        return render(
+            request, "diary/index.html", {"datetime_now": datetime_now})
 
 index = IndexView.as_view()
